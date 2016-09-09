08ae654ab719b214325b2c35205f30d0aaad4ae7
Maxime Bourget <bmx007@gmail.com>
2016-09-09 22:57:35 +0100

    Find configuration file in current directory branch.

--- Main.hs
+++ Main.hs
@@ -1,4 +1,25 @@
 module Main where
+import System.Environment (getArgs, getProgName)
+import System.Directory (doesFileExist, getCurrentDirectory)
+import System.FilePath (splitDirectories, joinPath, normalise)
+
+--  find path in the current directory or above
+findUp :: FilePath -> FilePath -> IO (Maybe FilePath)
+findUp path cwd  = go (splitDirectories (normalise cwd))
+  where go dirs = do 
+          let path' = joinPath (dirs ++ [path])
+          found <- doesFileExist path'
+          case (found, dirs) of
+            (True, _) -> return (Just path')
+            (False, []) -> return Nothing
+            (False, _) -> go (init dirs)
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  arg0 <- getProgName
+  args <- getArgs
+
+  configPath <- findUp ".csw" =<< getCurrentDirectory
+  print configPath
+  print arg0
+  
