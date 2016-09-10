224f52f021ddb30e41d39a8c16da60957a839cc5
Maxime Bourget <bmx007@gmail.com>
2016-09-10 17:21:51 +0100

    Read Yaml , compiles but not tested

--- Main.hs
+++ Main.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE DeriveGeneric #-}
 module Main where
+
 import Data.Maybe (maybeToList, fromMaybe)
+import Data.Yaml.Config (loadYamlSettings, useEnv)
 import System.Environment (getArgs, getProgName)
 import System.Directory (doesFileExist, getCurrentDirectory)
 import System.FilePath (splitDirectories, joinPath, normalise)
 import System.Process (callProcess)
+import GHC.Generics
+import Data.Aeson
 
 --  find path in the current directory or above
 findUp :: FilePath -> FilePath -> IO (Maybe FilePath)
@@ -20,15 +25,24 @@ data Config = Config { progName :: String -- ^ program name
   , cmd :: Maybe String -- ^ command to substitue with
   , subcommands :: [(String, String)]
   , translations :: [(String, String)]
-  }
+  } deriving (Generic)
+
+instance FromJSON Config
 
 -- ^ find an load the configuration for the given command
 -- lookup for the ".cws" file in current file or up (recursively)
 -- and find the corresponding entry
 loadConfig :: String -> IO (Maybe Config)
-loadConfig command = return . Just $ Config "pipo" Nothing [] []
+loadConfig command = do
+    cwd <- getCurrentDirectory
+    pathM <- findUp command cwd
+    case pathM of
+      Nothing -> return Nothing
+      Just path -> do
+        configs <- loadYamlSettings [path] [] useEnv
+        return (lookup command configs)
 
-
+  
 translate :: Config -> String -> [String] -> (String, [String])
 translate config command args = let
   command = fromMaybe command (cmd config)
